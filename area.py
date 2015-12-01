79c95dcdd6e1190afc2b6b7dacb067e2e267af97
Peter Stenger <peter.a.stenger@gmail.com>
2015-11-30 21:33:39 -0500

    save

--- area.py
+++ area.py
@@ -38,8 +38,56 @@ def solve_trapazoid():
     answer = int()
     answer = x * y/2 * z
     print("Your answer is : " + str(answer))
-def system():
-
+def solve_system():
+    def intro():
+        print(c.clear + c.green +"Welcome to the Math System Solver!")
+        print("y = 1y,x = 1x,")
+        print("No decimals, fractions, or 0/undefined slopes.")
+        time.sleep(3)
+        """DIRECTIONS"""
+    def p1():
+        print(c.clear + c.x + c.base03 + "Make sure your equation is in slope-intercept form.")
+        print("Enter y value please. Then hit enter.")
+        print(c.green +" ?" + c.base03 + "y = ? x + ?")
+    def p2():
+        print(c.clear + "Enter x value please. Then hit enter.")
+        print(a1 + "y = " + c.green + "?" + c.base03 + "x + ?")
+    def p3():
+        print(c.clear + "Enter last number. Then hit enter.")
+        print(a1 + "y = " + a2 + "x + " + c.green + "?" + c.base03)
+    def p4():
+        print(c.clear + "Solving the system:")
+        print(a1," y = ",a2," x + ",a3)
+    def p5():
+        print("slope is",x1)
+        print("y int is 0 ,",b1)
+    def p6():
+        print("x intercept is",b2,", 0")
+        print("3 points to plot are:0 ,",b1,"and",b3,",",b4,"and",b5,",",b6)
+        print("This is direct variation.")
+    if __name__ == "__main__":
+        intro()
+        p1()
+        a1 = input(">>> ")
+        p2()
+        a2 = input(">> ")
+        p3()
+        a3 = input("> ")
+        y = int(a1)
+        x = int(a2)
+        if a3 == "0":
+            b3 = int()
+            b4 = int()
+            b3 = y
+            b4 = x
+            b = int(a3)
+            p4()
+            x1 = int()
+            b1 = int()
+            x1 = 1. * x / y
+            b1 = 1. * b / y
+            p5()
+            x2 = int()
 def parse_args():
     import sys
     option = ""
