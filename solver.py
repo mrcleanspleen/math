2623cfc788b07415d155fe97b24e4e479604d93f
Alexander Green <aeg0322@gmail.com>
2015-12-03 17:01:03 -0500

    save

--- solver.py
+++ solver.py
@@ -4,17 +4,16 @@ import math
 
 def solve_cube():
     """Finds the volume of a cube"""
-    x = input("What is the height of your cube? >>> ")
-    x = float()
-    answer = float(x**2)
+    height = input("What is the height of your cube? >>> ")
+    x = int(height)
+    answer = x**3
     print("Your answer is : " ,answer)
 
 def solve_sphere():
     """Finds the volume of a sphere based on user input"""
-    x = input("What is the radius of your sphere? >>> ")
-    x = int()
-    xpower = math.pow(x, 3)
-    answer = int(4/3 * 3.14 * xpower)
+    radius = input("What is the radius of your sphere? >>> ")
+    x = int(radius)
+    answer = int(4/3 * 3.14 * x**3)
     print("Your answer is : ", answer)
 
 def solve_square():
