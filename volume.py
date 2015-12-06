f9551f65d227fa407c2f5d2e1c073d0b4ca83083
Alexander Green <aeg0322@gmail.com>
2015-12-06 14:59:09 -0500

    save

--- volume.py
+++ volume.py
@@ -2,7 +2,21 @@
 import skilstak.colors as c 
 import math
 import time as t
-
+def solve_cone():
+    """Finds the volume of a cone"""
+    height = input(c.cl + "What is the height of your cone? >>> ")
+    radius = input(c.cl + "What is the radius of the base of your cone? >>> ")
+    x = int(height)
+    y = int(radius)
+    answer = 3.14 * y**2 * x/3
+    print(c.base03 + "Your volume is : ",answer)
+def solve_sqpyramid():
+    base = input(c.cl + "What is the length of the base of your pyramid? >>> ")
+    height = input(c.cl + "What is the height of your pyramid? >>> ")
+    x = int(base)
+    y = int(height)
+    answer = x**2 * y/3
+    print(c.cl + "Your answer is : ", answer)
 def solve_cube():
     """Finds the volume of a cube"""
     height = input("What is the height of your cube? >>> ")
