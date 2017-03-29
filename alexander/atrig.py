9f971f118c847b16f005c0a595dd8c7e0b329742
Alexander Green <aeg0322@gmail.com>
2017-03-29 09:35:33 -0400

    save

---
+++ alexander/atrig.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import math as m
+print("""
+s = sin 
+c = cos
+t = ta            /|
+                 / |
+hypotenuse >>>  /  | <<< opposite
+               /   |
+              /____|        
+                adjacent
+
+sin = opposite / hypotenuse
+cos = adjacent / hypotenuse
+tan = opposite / adjacent
+        """)
+quest = input("Are you trying to find an angle or a side? ")
+if "side" in quest:
+    trig = input("cos, sin, or tan? ")
+if "angl
