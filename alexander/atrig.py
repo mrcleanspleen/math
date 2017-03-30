8fc4470607e6a968671de5ddfb6a9518c4d73bb5
Alexander Green <aeg0322@gmail.com>
2017-03-29 20:47:59 -0400

    save

--- alexander/atrig.py
+++ alexander/atrig.py
@@ -17,4 +17,24 @@ tan = opposite / adjacent
 quest = input("Are you trying to find an angle or a side? ")
 if "side" in quest:
     trig = input("cos, sin, or tan? ")
-if "angl
+    if trig == "cos":
+        adj = input("adjacent? ")
+        hyp = input("hypotenuse? ")
+if "angle" in quest:
+    inv = input("inverse of cos, sin, or tan? ")
+    if inv == "cos":
+        adj = float(input("adjacent? "))
+        hyp = float(input("hypotenuse? "))
+        deg = m.degrees(m.acos(adj/hyp))
+        print("x = " + str(deg))
+    if inv == "sin":
+        opp = float(input("opposite? "))
+        hyp = float(input("hypotenuse? "))
+        deg = m.degrees(m.asin(opp/hyp))
+        print("x = " + str(deg))
+    if inv == "tan":
+        opp = float(input("opposite? "))
+        adj = float(input("adjacent? "))
+        deg = m.degrees(m.atan(opp/adj))
+        print("x = " + str(deg))
+      
