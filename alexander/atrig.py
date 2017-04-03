4bae7076eac19bf105e7568add975c9da0c7bcff
Alexander Green <aeg0322@gmail.com>
2017-04-02 20:19:02 -0400

    save

--- alexander/atrig.py
+++ alexander/atrig.py
@@ -3,7 +3,7 @@ import math as m
 print("""
 s = sin 
 c = cos
-t = ta            /|
+t = tan           /|
                  / |
 hypotenuse >>>  /  | <<< opposite
                /   |
@@ -17,15 +17,35 @@ tan = opposite / adjacent
 quest = input("Are you trying to find an angle or a side? ")
 if "side" in quest:
     trig = input("cos, sin, or tan? ")
+    ang = float(input("What is the measure of the angle? "))
     if "cos" in trig:
         side = input("Do you know hyp or adj? ")
         length = float(input("What is the length of that side? "))
         if "hyp" in side:
             sidlen = m.degrees(m.cos(ang)*length)
-        else:
-            pass
-
-if "angle" in quest:
+            print(str(sidlen))
+        elif "adj" in side:
+            sidlen = m.degrees(length/m.cos(ang))
+            print(str(sidlen))
+    elif "sin" in trig:
+        side = input("Do you know opp or hyp? ")
+        length = float(input("What is the legnth of that side? "))
+        if "hyp" in side:
+            sidlen = m.degrees(m.sin(ang)*length)
+            print(str(sidlen))
+        elif "opp" in side:
+            sidlen = m.degrees(length/m.sin(ang))
+            print(str(sidlen))
+    elif "tan" in trig:
+        side = input("Do you know opp or adj? ")
+        length = float(input("What is the legnth of that side? "))
+        if "adj" in side:
+            sidlen = m.degrees(m.tan(ang)*length)
+            print(str(sidlen))
+        elif "opp" in side:
+            sidlen = m.degrees(legnth/m.tan(ang))
+            print(str(sidlen))
+elif "angle" in quest:
     inv = input("inverse of cos, sin, or tan? ")
     if inv == "cos":
         adj = float(input("adjacent? "))
